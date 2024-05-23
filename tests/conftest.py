e1924489ac9ddbaa7b7aab2ede4c14d97c020d30
Jeel Dobariya <25039678-JeelDobariya2@users.noreply.replit.com>
2024-05-23 10:32:46 +0000

    Solve the import issue with sys.path

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+import sys
+import os
+path = os.path.abspath(".")
+sys.path.append(path)
--- tests/intregrated_test/calc_basic_test.py
+++ tests/intregrated_test/calc_basic_test.py
@@ -1,5 +1,8 @@
-from ...calc import calc_execute
-
+from Calc.calc import calc_execute
 
 def test_calc_whitespace_comptablity():
-    calc_execute("")
+    assert calc_execute("") == ""
+    assert calc_execute("   ") == ""
+    assert calc_execute("\t") == ""
+    assert calc_execute("\n") == ""
+    assert calc_execute("\n \t \n     \t") == ""
