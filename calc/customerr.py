4652114172f98791cc98508cc69523d08af16b23
Jeel Dobariya <25039678-JeelDobariya2@users.noreply.replit.com>
2024-05-29 08:11:49 +0000

    Improve error handling

--- calc/customerr.py
+++ calc/customerr.py
@@ -1,6 +1,8 @@
 class CalcException(Exception):
-    pass
-
+    def __init__(self, message: str = "Service is currently unavailable!!", name = "CalcException"):
+        self.name = name
+        self.message = message
+        super().__init__(self.message, self.name)
 
 class InvalidSyntaxError(CalcException):
     pass
