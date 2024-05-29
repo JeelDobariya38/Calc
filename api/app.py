bac6b3154e8108861ad3ded6fa3b477d9bd74faf
Jeel Dobariya <25039678-JeelDobariya2@users.noreply.replit.com>
2024-05-29 09:01:51 +0000

    Improve api docs

--- api/app.py
+++ api/app.py
@@ -4,9 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from calc import calc_execute
 from calc.customerr import CalcException
 from .model import Command
+from . import metadata
 
 
-app = FastAPI()
+app = FastAPI(
+    title=metadata.TITLE,
+    description=metadata.DESCRIPTION,
+    version=metadata.VERSION,
+    license_info=metadata.LICENSE_INFO,
+    openapi_tags=metadata.METADATA_TAGS
+)
 
 origins = [
     "http://localhost",
@@ -23,27 +30,27 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+@app.get("/", tags=["Frontend"])
 def root():
     return FileResponse("website/index.html")
 
 
-@app.get("/style")
+@app.get("/style", tags=["Frontend"])
 def style():
     return FileResponse("website/style.css")
 
 
-@app.get("/script")
+@app.get("/script", tags=["Frontend"])
 def script():
     return FileResponse("website/script.js")
 
 
-@app.get("/heath")
+@app.get("/heath", tags=["Internal"])
 def health():
     return "Healthy"
 
 
-@app.post("/execute")
+@app.post("/execute", tags=["Calc"])
 def execute(_command: Command, response: Response):
     try:
         res = calc_execute(_command.command)
---
+++ api/metadata.py
@@ -0,0 +1,27 @@
+TITLE = "Calc - Math Interpreter"
+DESCRIPTION = """
+Calc API helps you do math related stuff. 🚀
+"""
+VERSION = "0.1.0"
+LICENSE_INFO = {
+    "name": "MIT",
+    "identifier": "MIT",
+}
+METADATA_TAGS = [
+    {
+        "name": "Calc",
+        "description": "Use for Interacting with Calc App",
+        # "externalDocs": {
+        #     "description": "Calc Docs",
+        #     "url": "https://fastapi.tiangolo.com/",
+        # },
+    },
+    {
+        "name": "Internal",
+        "description": "use for specific purpose",
+    },
+    {
+        "name": "Frontend",
+        "description": "use for website",
+    },
+]
